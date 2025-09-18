d73f024d38b810c33f103c27b6ae14c77298e986
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-18 20:51:17 +0300

    short training, agent sits still

--- watch.py
+++ watch.py
@@ -1,6 +1,32 @@
 from stable_baselines3 import PPO
 import gymnasium as gym
 from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
 
-def watch(env_path ="./env/", timesteps=5):
-    
+
+def watch(
+    env_path="./env/vecnorm.pkl",
+    model_path="./model/best_model.zip",
+    env_name="HalfCheetah-v5",
+    timesteps=5,
+):
+    env = DummyVecEnv([lambda: Monitor(gym.make(env_name, render_mode="human"))])
+    env = VecNormalize.load(env_path, env)
+    env.norm_reward = False
+    env.training = False
+
+    model = PPO.load(model_path, env)
+
+    rewards, lengths = evaluate_policy(
+        model, env, timesteps, render=True, return_episode_rewards=True
+    )
+
+    for i in range(len(rewards)):
+        print(f"run={i + 1}, reward={rewards[i]}, length={lengths[i]}")
+
+    env.close()
+
+
+if __name__ == "__main__":
+    watch()
